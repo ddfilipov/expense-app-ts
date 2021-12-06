4a2bb02c684b7ccc17c47148070194a76f08c3d8
ddfilipov <denisfilipov12+3@gmail.com>
2021-12-06 17:52:21 +0100

    Lesson 49 - Working with State

--- src/components/Expenses/ExpenseItem.tsx
+++ src/components/Expenses/ExpenseItem.tsx
@@ -2,6 +2,7 @@ import "./ExpenseItem.css";
 import { FC } from "react";
 import { ExpenseDate } from "./ExpenseDate";
 import { Card } from "../UI/Card";
+import { useState } from "react";
 
 export interface ExpenseItemProps {
     id: string;
@@ -11,14 +12,16 @@ export interface ExpenseItemProps {
 }
 
 const ExpenseItem: FC<ExpenseItemProps> = (props) => {
+    const [title, setTitle] = useState(props.title);
+
     const clickHandler = () => {
-        console.log("clicking");
+        setTitle("new title");
     };
     return (
         <Card classCSS="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
             <button onClick={clickHandler}>Change Title</button>
