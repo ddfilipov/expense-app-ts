d0a9b596a0cc223ac7e1b1d90d08482ad03f41ed
ddfilipov <denisfilipov12+3@gmail.com>
2021-12-14 17:38:06 +0100

    Lesson 61 - Controlled vs Uncontrolled Components & Stateless vs Stateful Components

--- src/components/Expenses/ExpenseItem.tsx
+++ src/components/Expenses/ExpenseItem.tsx
@@ -12,19 +12,14 @@ export interface ExpenseItemProps {
 }
 
 const ExpenseItem: FC<ExpenseItemProps> = (props) => {
-    const [title, setTitle] = useState(props.title);
 
-    const clickHandler = () => {
-        setTitle("new title");
-    };
     return (
         <Card classCSS="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
-            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 };
