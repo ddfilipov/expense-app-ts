e007a78573877c98fae8b2c9beb476b78dab8f05
ddfilipov <denisfilipov12+3@gmail.com>
2021-12-16 21:02:47 +0100

    lame typo corrected

--- src/App.tsx
+++ src/App.tsx
@@ -3,32 +3,33 @@ import { NewExpense } from "./components/NewExpense/NewExpense";
 import { ExpenseItemProps } from "./components/Expenses/ExpenseItem";
 import { useState } from "react";
 
+const initialExpenses = [
+    {
+        id: "e1",
+        title: "Toilet Paper",
+        amount: 94.12,
+        date: new Date("2020-07-14"),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date("2021-02-12") },
+    {
+        id: "e3",
+        title: "Car Insurance",
+        amount: 294.67,
+        date: new Date("2021-02-28"),
+    },
+    {
+        id: "e4",
+        title: "New Desk (Wooden)",
+        amount: 450,
+        date: new Date("2021-05-12"),
+    },
+];
+
 function App() {
-    const expensesList = [
-        {
-            id: "e1",
-            title: "Toilet Paper",
-            amount: 94.12,
-            date: new Date("2020-07-14"),
-        },
-        { id: "e2", title: "New TV", amount: 799.49, date: new Date("2021-02-12") },
-        {
-            id: "e3",
-            title: "Car Insurance",
-            amount: 294.67,
-            date: new Date("2021-02-28"),
-        },
-        {
-            id: "e4",
-            title: "New Desk (Wooden)",
-            amount: 450,
-            date: new Date("2021-05-12"),
-        },
-    ];
-    const [expenses, setExpanses] = useState(expensesList);
+    const [expenses, setExpanses] = useState(initialExpenses);
 
     const addExpenseHandler = (expense: ExpenseItemProps) => {
-        setExpanses([...expenses, expense]);
+        setExpanses([expense, ...expenses]);
     };
 
     return (
--- src/components/Expenses/ExpenseDate.tsx
+++ src/components/Expenses/ExpenseDate.tsx
@@ -12,9 +12,9 @@ export const ExpenseDate: FC<ExpenseDateType> = ({ date }) => {
 
     return (
         <div className="expense-date">
-            <div className="expanse-date__month">{month}</div>
-            <div className="expanse-date__year">{year}</div>
-            <div className="expanse-date__day">{day}</div>
+            <div className="expense-date__month">{month}</div>
+            <div className="expense-date__year">{year}</div>
+            <div className="expense-date__day">{day}</div>
         </div>
     );
 };
